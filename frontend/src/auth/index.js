e9fed567db0eba2ff38e488e222245a96651c8e0
Manansh Arora <44532652+manansharora@users.noreply.github.com>
2023-08-04 14:04:02 +0000

    backend cors added

--- frontend/src/auth/index.js
+++ frontend/src/auth/index.js
@@ -1,3 +1,43 @@
+import { API } from "../backend";
+
+export const signup = (user) => {
+  return fetch(`${API}user/`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+export const signin = (user) => {
+    const formData = new FormData();
+  
+    for (const name in user) {
+      console.log(user[name]);
+      formData.append(name, user[name]);
+    }
+  
+    for (var key of formData.keys()) {
+      console.log("MYKEY: ", key);
+    }
+  
+    return fetch(`${API}user/login/`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        console.log("SUCCESS", response);
+        return response.json();
+      })
+      .catch((err) => console.log(err));
+  };
+
 export const isAuthenticated = () => {
     
     return false;
--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -2,7 +2,16 @@ import React, { Component } from 'react'
 import Base from './Base';
 import Menu from './Menu';
 
+// import axios from "axios";
+
+// const findPets = () => {
+//   const pets = axios.get(`https://miniature-disco-4wqrpjwx6v5cjv56-8080.app.github.dev/clients`);
+//   return pets;
+// };
+
 export default function Home() {
+  // const pet = findPets();
+  // console.log(pet);
 
   return (
     <Base title="Bonds" description="Explore And Invest">
