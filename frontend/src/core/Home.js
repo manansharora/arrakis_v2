ccaf84632c9744d5412633b61987dba4c8784a28
Manansh Arora <44532652+manansharora@users.noreply.github.com>
2023-08-06 19:41:19 +0000

    cleaned useless objects

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 import Base from './Base';
 import Menu from './Menu';
 
-// import axios from "axios";
+import axios from "axios";
 
-// const findPets = () => {
-//   const pets = axios.get(`https://miniature-disco-4wqrpjwx6v5cjv56-8080.app.github.dev/clients`);
-//   return pets;
-// };
+const findPets = () => {
+  const pets = axios.get(`https://miniature-disco-4wqrpjwx6v5cjv56-8080.app.github.dev/clients`);
+  return pets;
+};
 
 export default function Home() {
-  // const pet = findPets();
-  // console.log(pet);
+  const pet = findPets();
+  console.log(pet);
 
   return (
     <Base title="Bonds" description="Explore And Invest">
