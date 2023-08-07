20e90a19f2169ffe16b8d2aa1f6a1fe34669520a
manansh <manansh.arora@gmail.com>
2023-08-07 12:14:48 +0530

    added admin-dashboard

---
+++ frontend/src/core/trades/Trades.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+import Header from "./components/AppHeader";
+import AppChart from "./components/AppChart";
+import AppTable from "./components/AppTable";
+
+import "./App.css";
+import data from "./constants/data";
+
+function Trades() {
+    const [responseData, setResponseData] = useState("");
+    const [value, setValue] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const url = `http://fetest.pangeatech.net/data`;
+        const headers = {
+            "Content-Security-Policy": "upgrade-insecure-requests",
+        };
+        try {
+            const response = await fetch(url, headers);
+            setResponseData(await response.json());
+        } catch (error) {
+            setResponseData(data);
+            throw new Error("Failed to fetch data!");
+        }
+    };
+    return responseData ? renderApp(responseData, setValue, value) : <></>;
+}
+
+export default Trades;
+
+function renderApp(responseData, setValue, value) {
+    return (
+        <div>
+            <Header
+                data={responseData}
+                filterData={setValue}
+                filterInitialData={value}
+            />
+            <div className="flex flex-col">
+                <div className="flex justify-center my-5 h-[100%] w-[100%]">
+                    <AppChart data={responseData} selectedFilter={value} />
+                </div>
+                <div className="px-20 py-10">
+                    <AppTable data={responseData} selectedFilter={value} />
+                </div>
+            </div>
+        </div>
+    );
+}
